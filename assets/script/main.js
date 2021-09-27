9a65614e7762c6755e1546ea27640127fb75a921
Carlos Regalado <cd.regaladoaguirre@hotmail.com>
2021-09-27 13:45:28 -0500

    Added the toggle functionality

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,36 +1,51 @@
 "use strict"
 
 const canvas = document.querySelector(".container");
-const width = document.querySelector(".container").clientWidth;
-const height = document.querySelector(".container").clientHeight;
 const clearBtn = document.getElementById("clear");
 const color = document.getElementById("color-picker");
-const canvasSize = document.getElementById("grid-selector");
+const canvasGrid = document.getElementById("grid-selector");
+const eraserBtn = document.getElementById("eraser");
+const rainbowBtn = document.getElementById("rainbow");
+const shadeBtn = document.getElementById("shade");
+const lightBth = document.getElementById("light");
 
 function grid(rows, cols){
     for(let i = 0; i < (rows * cols); i++){
         let cell = document.createElement("div");
         canvas.appendChild(cell).className = "cell";
-        cell.style.width = `${width/cols}px`;
-        cell.style.height = `${height/rows}px`;
+        cell.style.width = `${canvas.clientWidth/cols}px`;
+        cell.style.height = `${canvas.clientHeight/rows}px`;
     }
 }
+
 
 grid(16,16);
 
 const cell = document.querySelectorAll(".cell");
 
-cell.forEach(item => item.addEventListener("mouseover", colorOver))
+cell.forEach(item => item.addEventListener("mouseover", paint))
 
-function colorOver(e){
-    e.target.style["background-color"] = "black";
-    e.target.style.color = "black";
+function paint(e){
+
+    e.target.style["background-color"] = color.value;
 }
 
 function colorReset(e){
     e.target.style["background-color"] = "#fff";
 }
 
+function toggle(e){
+    if(e.target.value == "off"){
+        e.target.value = "on";
+        e.target.classList.add("on")
+    } else if(e.target.value == "on"){
+        e.target.value = "off"
+        e.target.classList.remove("on")
+    }
+}
+
 clearBtn.addEventListener("click", () =>{
     cell.forEach(item => item.style["background-color"] = "#fff");
 })
+
+eraserBtn.addEventListener("click", toggle);
